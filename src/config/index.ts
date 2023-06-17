ca0e99d6fd8c1bde09dad93604deb03a1e4d5447
fxckcode <diegoale8582@hotmail.com>
2023-06-17 17:09:35 -0500

    optimize

---
+++ src/config/index.ts
@@ -0,0 +1,26 @@
+export const skills = [
+	{
+		tecnologi: "HTML",
+		progress: 95,
+	},
+	{
+		tecnologi: "CSS",
+		progress: 90
+	},
+	{
+		tecnologi: "JS",
+		progress: 80
+	},
+	{
+		tecnologi: "PHP",
+		progress: 80
+	},
+	{
+		tecnologi: "PYTHON",
+		progress: 90
+	},
+	{
+		tecnologi: "BASH",
+		progress: 50
+	}
+];
